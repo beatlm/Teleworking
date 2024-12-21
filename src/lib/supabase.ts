7c866fdbfede4d833bb41c92c1ec9e7214523748
Beatriz López Moreno <beatlm@gmail.com>
2024-12-21 13:40:06 +0100

    Variables entorno

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { format, parseISO } from 'date-fns';
 
-const supabaseUrl = 'https://eraqeukkrdzisvpaxbiz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyYXFldWtrcmR6aXN2cGF4Yml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM0OTYxMDYsImV4cCI6MjA0OTA3MjEwNn0.4H9klDS5mxhI-pHsMM9MvSeMPit5yBTSouBPVuKmAzk';
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error(
